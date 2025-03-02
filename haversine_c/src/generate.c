77fa3ead1a5d4ed82ad7152adf9617a9624d7d88
telesphore <telesphore4@gmail.com>
2025-03-02 14:14:59 -0500

    Parse args

--- haversine_c/src/generate.c
+++ haversine_c/src/generate.c
@@ -1,13 +1,83 @@
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "abbrev.h"
 
-int main(int argc, char** argv) {
-    printf("%d \n", argc);
+typedef struct Args {
+    u32 count;
+    f64 lat_lo;
+    f64 lat_hi;
+    f64 lon_lo;
+    f64 lon_hi;
+    bool error;
+} Args;
 
-    for (i32 i = 0; i < argc; ++i) {
-        printf("%s\n", argv[i]);
+Args parse_args(int argc, char **argv) {
+    Args args = {
+        .lat_lo = -90.0,
+        .lat_hi = 90.0,
+        .lon_lo = -180.0,
+        .lon_hi = 180.0,
+        .error = false,
+    };
+    i8 opt;
+    char *end;
+
+    while ((opt = getopt(argc, argv, "c:a:A:o:O:")) != -1) {
+        switch (opt) {
+            case 'c':
+                args.count = strtoul(optarg, &end, 10);
+                if (optarg == end || *end != '\0') {
+                    printf("-c count is not an integer > 0 '%s'\n", optarg);
+                    args.error = true;
+                }
+                break;
+
+            case 'a':
+                args.lat_lo = strtof(optarg, &end);
+                if (optarg == end || *end != '\0') {
+                    printf("-a lat low is not a float '%s'\n", optarg);
+                    args.error = true;
+                }
+                break;
+
+            case 'A':
+                args.lat_hi = strtof(optarg, &end);
+                if (optarg == end || *end != '\0') {
+                    printf("-A lat high is not a float '%s'\n", optarg);
+                    args.error = true;
+                }
+                break;
+
+            case 'o':
+                args.lon_lo = strtof(optarg, &end);
+                if (optarg == end || *end != '\0') {
+                    printf("-o lon low is not a float '%s'\n", optarg);
+                    args.error = true;
+                }
+                break;
+
+            case 'O':
+                args.lon_hi = strtof(optarg, &end);
+                if (optarg == end || *end != '\0') {
+                    printf("-O lon high is not a float '%s'\n", optarg);
+                    args.error = true;
+                }
+                break;
+        }
     }
+    return args;
+}
+
+int main(int argc, char **argv) {
+    Args args = parse_args(argc, argv);
+    if (args.error) {
+        return 1;
+    }
+
+    printf("%d %f %f %f %f %d\n", args.count, args.lat_lo, args.lat_hi, args.lon_lo, args.lon_hi, args.error);
 
     return 0;
 }
--- haversine_c/src/haversine.c
+++ haversine_c/src/haversine.c
@@ -1,48 +1,33 @@
 #ifndef HAVERSINE_C
 #define HAVERSINE_C
 
-//#include <math.h>
+#include <math.h>
+
 #include "abbrev.h"
 
 const f64 earth_radius = 6372.8;
 
+typedef struct Haversine {
+    f64 lon1, lat1;
+    f64 lon2, lat2;
+} Haversine;
 
-typedef struct haversine {
-    f64 lng1, lat1;
-    f64 lng2, lat2;
-} haversine;
+f64 square(f64 v) { return v * v; }
 
+f64 deg2rad(f64 deg) { return 0.01745329251994329577 * deg; }
 
-f64 square(f64 x) {
-    return x * x;
+f64 haversine_0(f64 lon1, f64 lat1, f64 lon2, f64 lat2, f64 radius) {
+    f64 dLat = deg2rad(lat2 - lat1);
+    f64 dLon = deg2rad(lon2 - lon1);
+    lat1 = deg2rad(lat1);
+    lat2 = deg2rad(lat2);
+
+    f64 a = square(sin(dLat / 2.0)) + cos(lat1) * cos(lat2) * square(sin(dLon / 2));
+    f64 c = 2.0 * asin(sqrt(a));
+
+    f64 result = radius * c;
+
+    return result;
 }
-
-
-f64 deg2rad(f64 deg) {
-    return 0.01745329251994329577 * deg;
-}
-
-
-
-/*static f64 startingHaversine(f64 X0, f64 Y0, f64 X1, f64 Y1, f64 EarthRadius)*/
-/*{*/
-/*    f64 lat1 = Y0;*/
-/*    f64 lat2 = Y1;*/
-/*    f64 lon1 = X0;*/
-/*    f64 lon2 = X1;*/
-/**/
-/*    f64 dLat = deg2rad(lat2 - lat1);*/
-/*    f64 dLon = deg2rad(lon2 - lon1);*/
-/*    lat1 = deg2rad(lat1);*/
-/*    lat2 = deg2rad(lat2);*/
-/**/
-/*    f64 a = square(sin(dLat/2.0)) + cos(lat1)*cos(lat2)*square(sin(dLon/2));*/
-/*    f64 c = 2.0*asin(sqrt(a));*/
-/**/
-/*    f64 Result = earth_radius * c;*/
-/**/
-/*    return Result;*/
-/*}*/
-
 
 #endif
